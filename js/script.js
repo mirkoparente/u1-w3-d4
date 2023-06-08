79f15be6cde3a9b7f8bdf1205c925017caf4932d
mirk89 <mirkopare@gmail.com>
2023-06-08 19:07:56 +0200

    aggiunta card giocatore

--- js/script.js
+++ js/script.js
@@ -4,21 +4,28 @@ const game = document.getElementById("btn");
 const game2 = document.getElementById("btn2");
 //creo la tabella
 const tabella = [];
-
 const rand = function () {
-    const random = Math.floor(Math.random(tabella.length) * 76);
-    console.log(random);
-    const extract = document.createElement("div");
-    extract.classList.add("numb");
-    extract.classList.add("select");
-    if (random !== 0) {
-      extract.innerText = random;
-      document.getElementById("numEstratto").appendChild(extract);
-    }
-    return random
-  };
+  const random = Math.floor(Math.random(tabella.length) * 76);
+  //   console.log(random);
+  const extract = document.createElement("div");
+  extract.classList.add("numb");
+  extract.classList.add("select");
+  if (random !== 0) {
+    extract.innerText = random;
+    document.getElementById("numberExtract").appendChild(extract);
+  }
+  
+  document.querySelectorAll(".tombola .numb").forEach((d) => {
+    let di = d.innerText;
 
-const createTable = function (random) {
+    console.log(di);
+    if (extract.innerText === di) {
+      d.classList.add("select");
+    }
+    
+  });
+};
+const createTable = function () {
   for (let i = 0; i < 76; i++) {
     //faccio apparire la tabella
     const startDiv = document.createElement("div");
@@ -28,22 +35,30 @@ const createTable = function (random) {
     document.getElementById("nTombola").appendChild(startDiv);
     game.classList.add("none");
     game2.classList.remove("none");
-
-    if(i===random){
-        startDiv.classList.remove("select")
-        console.log(startDiv);
+    const dispCard=document.getElementsByClassName("tombola")
+    for(let i=0;i<dispCard.length;i++){
+        dispCard[i].classList.remove("none")
     }
+    console.log();
   }
-  
 };
-
-
+const rndGame = [];
+const cardGame = function () {
+  for (let i = 0; i < 24; i++) {
+    rndGame.push(Math.floor(Math.random(i) * 76));
+    //faccio apparire la tabella giocatore
+    const startDiv = document.createElement("div");
+    startDiv.classList.add("numb");
+    startDiv.innerText = rndGame[i] + 1;
+    console.log(startDiv);
+    document.getElementById("card").appendChild(startDiv);
+  }
+};
 
 //random
 //
 
-
-
 game.addEventListener("click", createTable);
+game.addEventListener("click", cardGame);
 
 game2.addEventListener("click", rand);
